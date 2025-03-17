74d2af4434f33c0c4db0a5732b5bb8f943b35bbb
Nithish-BN <152054408+Nithish-BN@users.noreply.github.com>
2025-03-17 15:08:27 +0530

    Create circle.java

---
+++ circle.java
@@ -0,0 +1,14 @@
+public class circle{
+        public static void main (String [] args){
+        Scanner sc = new Scanner(System.in);
+         System.out.println("Enter The Radius :");
+         int r = sc.nextInt();
+
+         double circle = 0.5 * r * r;
+
+         System.out.println("Area : " + circle);
+
+
+
+
+        }
